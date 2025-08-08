3bf47d29a1f3631b3d6ab09c7b91ec90f41de5e7
slabwu <134193743+slabwu@users.noreply.github.com>
2025-08-08 17:48:22 +0800

    Test Vitest

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
 import App from './App';
-
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App title="React" />);
-
-    screen.debug();
-
-    // check if App components renders headline
-  });
-});
 
 describe('something truthy and falsy', () => {
   it('true to be true', () => {
@@ -20,5 +9,12 @@ describe('something truthy and falsy', () => {
 
   it('false to be false', () => {
     expect(false).toBe(false);
+  });
+});
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App/>);
+    screen.debug();
   });
 });
