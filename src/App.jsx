a8ca38e6f0acb11b684b5064fb543824470544e1
slabwu <134193743+slabwu@users.noreply.github.com>
2025-08-09 09:53:36 +0800

    Test snapshot

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import './App.css'
+import { useState } from "react";
 
-const App = () => <h1>Our First Test</h1>;
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
 
 export default App;
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,20 +1,23 @@
-import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
   });
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
-  });
-});
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
 
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App/>);
-    screen.debug();
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
